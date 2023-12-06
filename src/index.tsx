05efe83e993676231729510e425f24936791c226
rat-balls <delalande.ethan@gmail.com>
2023-12-06 14:15:56 +0100

    added firebase

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCrr1BLI30V-raHt0V7kZ0AkCs5OKryIfU",
+  authDomain: "movie-light-383dd.firebaseapp.com",
+  projectId: "movie-light-383dd",
+  storageBucket: "movie-light-383dd.appspot.com",
+  messagingSenderId: "860094022085",
+  appId: "1:860094022085:web:b8a678c7440bc70000cbac",
+  measurementId: "G-7G3MYDEVD5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+onAuthStateChanged(auth, user => {
+  if(user !== null) {
+    console.log('logged in !');
+  } else {
+    console.log('no user');
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
