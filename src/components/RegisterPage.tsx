f46c2d8183d69f35631d0c261ffb00f891ae21ce
Daadaa Nabil <103863031+RyzKum@users.noreply.github.com>
2023-12-07 17:59:18 +0100

    Register change

--- src/components/RegisterPage.tsx
+++ src/components/RegisterPage.tsx
@@ -2,7 +2,7 @@ import React, { FormEvent, useEffect, useState } from 'react'
 import './RegisterPage.scss'
 import { createUserWithEmailAndPassword, getAuth, updateProfile } from 'firebase/auth'
 import { app } from '..'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'; 
 
 export default function RegisterPage() {
   const navigate = useNavigate();
@@ -43,11 +43,12 @@ export default function RegisterPage() {
     <div className="register-page">
       <h1 className='NamePage'>REGISTER PAGE</h1>
       <form onSubmit={handleSubmit}>
-        <input className='UserName' type="text" placeholder="Nom d'utilisateur" value={userName} onChange={(e) => setUserName(e.target.value)} required />
+        <input className='UserName' type="text" placeholder="UserName" value={userName} onChange={(e) => setUserName(e.target.value)} required />
         <input type="email" placeholder="Email" value={userEmail} onChange={(e) => setUserEmail(e.target.value)} required />
-        <input type="password" placeholder="Mot de passe" value={userPswrd} onChange={(e) => setUserPswrd(e.target.value)} required />
-        <button type="submit">S'inscrire</button>
-      </form>
+        <input type="password" placeholder="Password" value={userPswrd} onChange={(e) => setUserPswrd(e.target.value)} required />
+        <button type="submit">Register</button>
+        </form>
+        <p>Already have an account? <Link to="/login">Log in here</Link></p>
     </div>
   )
 }
