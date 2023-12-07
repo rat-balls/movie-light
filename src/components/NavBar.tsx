c3ff61f22a9d8d761d267d3fa0ae826958335983
rat-balls <delalande.ethan@gmail.com>
2023-12-07 13:54:32 +0100

    updated Navbar to be dynamic with logged in or out

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './NavBar.scss'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { app } from '..';
 
 function NavBar() {
+  const auth = getAuth(app);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  onAuthStateChanged(auth, (user) => {
+    if(user) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  })
+
   return (
     <div className="navbar">
-      <Link to="/">Home</Link> 
+      <Link to="/">Home</Link>
+      {!loggedIn ? 
+      <>
       <Link to="/register">Register</Link> 
       <Link to="/login">Login</Link>
-      <Link to="/user">User</Link>
-    </div>
+      </> 
+      : 
+      <>
+        <Link to="/user">User</Link>
+        <Link to="/logout">Logout</Link>
+      </>}
+      </div>
   )
 }
 
