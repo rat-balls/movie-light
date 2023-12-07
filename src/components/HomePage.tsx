9e3f01534a7f85a791456db767739f80141ae712
rat-balls <delalande.ethan@gmail.com>
2023-12-07 16:49:04 +0100

    changed movies to series

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -10,101 +10,105 @@ function HomePage() {
 
   const [clicked, setClicked] = useState(false);
 
-  type movieType = {
+  type seriesType = {
       adult: boolean,
       backdrop_path: string,
+      first_air_date: string,
       genre_ids: number[],
       id: number,
+      name: string,
+      origin_country: string[],
       original_language: string,
-      original_title: string,
+      original_name: string,
       overview: string,
       popularity: number,
       poster_path: string,
-      release_date: string,
-      title: string,
       video: boolean,
       vote_average: number,
       vote_count: number,
       clicked : boolean
   }
 
-  const mvList: movieType[] = [];
+  const srList: seriesType[] = [];
 
-  const [movieList, setMovieList] = useState(mvList)
-  const [filteredMovieList, setFilteredMovieList] = useState(mvList)
+  const [seriesList, setSeriesList] = useState(srList)
+  const [filteredSeriesList, setFilteredSeriesList] = useState(srList)
 
   const API_KEY='1321ab72499d42466b65c40a21df1192'
-  const url = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+  const url = `https://api.themoviedb.org/3/tv/popular?api_key=${API_KEY}`
 
   useEffect(() => {
     async function fetchInfo() { 
-      const res = await fetch(url)
-      const d = await res.json()
-      d.results.filter((movie: movieType) => (movie.title == 'Napoleon' ? movie.title = 'Nabileon' : movie.title = movie.title))
-      setMovieList(d.results)
+      try {
+        const res = await fetch(url);
+        const d = await res.json();
+        setSeriesList(d.results);
+      } catch (error) {
+        console.log(error);
+      }
     }
     fetchInfo();
   }, [url])
 
-  const handleAdd = (movieId: number) => {
-    const updatedMovies = movieList.map((movie: movieType) => {
-      if (movie.id === movieId) {
-        return { ...movie, clicked: !movie.clicked };
+  const handleAdd = (serieId: number) => {
+    const updatedSeries = seriesList.map((serie: seriesType) => {
+      if (serie.id === serieId) {
+        return { ...serie, clicked: !serie.clicked };
       }
-      return movie;
+      return serie;
     });
-    setMovieList(updatedMovies);
+    setSeriesList(updatedSeries);
   };
 
 
   return (
-<div className="home-page">
-  {movieList.length > 0 && (
-    <div 
-      className="featured-movie" 
-      style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280/${movieList[0].poster_path})` }}
-    >
-      <div className="gradient-overlay"></div>
-      <div className="text-content">
-        <div className="overlay">
-          <p className='TitleOverlay'>{movieList[0].title}</p>
-          <p className='OverviewOverlay'>{movieList[0].overview.substring(0, 225).concat('...')}</p>
-          <p className='OptionOverlay'>
-            <span className="bubble">{movieList[0].release_date}</span>
-          </p>
-        </div>
-      </div>
-    </div>
-  )}
-      <form className='FormSearch'>
-        <div className="search-bar">
-          <span className="search-icon"><FontAwesomeIcon icon={faSearch} /></span>
-          <input
-            type="text"
-            placeholder="Search"
-            name="query"
-          />
-        </div>
-      </form>
-    <div className="movie-list">
-      {movieList.map((movie: movieType, index) => (
-        <div className="movie-item" key={index}>
-          <div className="movie-poster">
-            <img className="movie-image" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
-            <button
-                className={`add-button ${movie.clicked ? 'clicked' : ''}`}
-                onClick={() => handleAdd(movie.id)}>
-                {movie.clicked ? '✓' : '+'}
-              </button>
+  <div className="home-page">
+    {seriesList.length > 0 && (
+      <div 
+        className="featured-serie" 
+        style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w1280/${seriesList[0].poster_path})` }}
+      >
+        <div className="gradient-overlay"></div>
+        <div className="text-content">
+          <div className="overlay">
+            <p className='TitleOverlay'>{seriesList[0].name}</p>
+            <p className='OverviewOverlay'>{seriesList[0].overview.substring(0, 225).concat('...')}</p>
+            <p className='OptionOverlay'>
+              <span className="bubble">{seriesList[0].popularity}</span>
+            </p>
           </div>
-          <p className="TitleMovie">
-            {movie.title.length > 22 ? movie.title.substring(0, 22).concat('...') : movie.title}
-          </p>
         </div>
-      ))}
       </div>
-    </div>
-  );
+    )}
+        <form className='FormSearch'>
+          <div className="search-bar">
+            <span className="search-icon"><FontAwesomeIcon icon={faSearch} /></span>
+            <input
+              type="text"
+              placeholder="Search"
+              name="query"
+            />
+          </div>
+        </form>
+      <div className="serie-list">
+        {seriesList.map((serie: seriesType, index) => (
+          <div className="serie-item" key={index}>
+            <div className="serie-poster">
+              <img className="serie-image" src={`https://image.tmdb.org/t/p/w500/${serie.poster_path}`} alt={serie.name} />
+              <button
+                  className={`add-button ${serie.clicked ? 'clicked' : ''}`}
+                  onClick={() => handleAdd(serie.id)}>
+                  {serie.clicked ? '✓' : '+'}
+                </button>
+            </div>
+            <p className="TitleSerie">
+              {serie.name.length > 22 ? serie.name.substring(0, 22).concat('...') : serie.name}
+            </p>
+          </div>
+        ))}
+        </div>
+      </div>
+    );
 }
 
 
