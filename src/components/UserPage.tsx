613ff04099adaaab78fe88d49ac0b27d17b8c409
rat-balls <delalande.ethan@gmail.com>
2023-12-07 13:50:21 +0100

    added 'dynamic' username in user page

--- src/components/UserPage.tsx
+++ src/components/UserPage.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import './UserPage.scss'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { app } from '..';
 
 function UserPage() {
-  const userName = "FARTHAN DELAFART"; 
+  const [displayName, setDisplayName] = useState('');
+  const auth = getAuth(app);
+  const user = auth.currentUser;
 
+  onAuthStateChanged(auth, (user) => {
+    if(user && user.displayName !== null) {
+      setDisplayName(user.displayName);
+    }
+  })
+  
   return (
     <div className="user-page">
       <FontAwesomeIcon icon={faUser} className="profile-icon" />
-      <h1 className="user-name">{userName}</h1>
+      <h1 className="user-name">{displayName}</h1>
     </div>
   )
 }
